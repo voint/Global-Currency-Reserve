d05e2eb4500290f8637553dc569be99bab050ab0
voint <35273955+voint@users.noreply.github.com>
2019-01-01 12:07:14 +0200

    Update miner.cpp

--- src/miner.cpp
+++ src/miner.cpp
@@ -524,7 +524,7 @@ void StakeMiner(CWallet *pwallet)
     SetThreadPriority(THREAD_PRIORITY_LOWEST);
 
     // Make this thread recognisable as the mining thread
-    RenameThread("diplexcoin-miner");
+    RenameThread("Global-Currency-Reserve-miner");
 
     bool fTryToSync = true;
 

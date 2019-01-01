30fc6ee4cd85ea3d08c96f62723c270cbeb3af44
voint <35273955+voint@users.noreply.github.com>
2019-01-01 11:46:18 +0200

    Update qtipcserver.h

--- src/qt/qtipcserver.h
+++ src/qt/qtipcserver.h
@@ -1,8 +1,8 @@
 #ifndef QTIPCSERVER_H
 #define QTIPCSERVER_H
 
-// Define diplexcoin-Qt message queue name
-#define BITCOINURI_QUEUE_NAME "diplexcoinURI"
+// Define Global-Currency-Reserve-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "Global-Currency-ReserveURI"
 
 void ipcScanRelay(int argc, char *argv[]);
 void ipcInit(int argc, char *argv[]);

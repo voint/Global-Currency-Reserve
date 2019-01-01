eef02be2b7ce96401f3773592c7439f78738e5b8
voint <35273955+voint@users.noreply.github.com>
2019-01-01 11:55:13 +0200

    Update optionsdialog.cpp

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -149,7 +149,7 @@ void OptionsDialog::setMapper()
     mapper->addMapping(ui->lang, OptionsModel::Language);
     mapper->addMapping(ui->unit, OptionsModel::DisplayUnit);
     mapper->addMapping(ui->displayAddresses, OptionsModel::DisplayAddresses);
-    mapper->addMapping(ui->usediplexcoinTheme, OptionsModel::UsediplexcoinTheme);	
+    mapper->addMapping(ui->useGlobal-Currency-ReserveTheme, OptionsModel::UseGlobal-Currency-ReserveTheme);	
     mapper->addMapping(ui->coinControlFeatures, OptionsModel::CoinControlFeatures);
 }
 
@@ -202,7 +202,7 @@ void OptionsDialog::showRestartWarning_Proxy()
 {
     if(!fRestartWarningDisplayed_Proxy)
     {
-        QMessageBox::warning(this, tr("Warning"), tr("This setting will take effect after restarting diplexcoin."), QMessageBox::Ok);
+        QMessageBox::warning(this, tr("Warning"), tr("This setting will take effect after restarting Global-Currency-Reserve."), QMessageBox::Ok);
         fRestartWarningDisplayed_Proxy = true;
     }
 }
@@ -211,7 +211,7 @@ void OptionsDialog::showRestartWarning_Lang()
 {
     if(!fRestartWarningDisplayed_Lang)
     {
-        QMessageBox::warning(this, tr("Warning"), tr("This setting will take effect after restarting diplexcoin."), QMessageBox::Ok);
+        QMessageBox::warning(this, tr("Warning"), tr("This setting will take effect after restarting Global-Currency-Reserve."), QMessageBox::Ok);
         fRestartWarningDisplayed_Lang = true;
     }
 }
